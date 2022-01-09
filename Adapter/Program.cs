f1f8f370ed2b9e54a7c8faab93b2812a548d4a6e
NaysuDaKema <69696032+NaysuDaKema@users.noreply.github.com>
2022-01-10 00:38:09 +0400

    Добавьте файлы проекта.

---
+++ Adapter/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Man man = new();
+            Boat boat = new();
+            man.Travel(boat);
+            Jet jet = new();
+
+            // используем адаптер
+            ITransport jetTransport = new HelToTransportAdapter(jet);
+            man.Travel(jetTransport);
+            Console.Read();
+        }
+    }
+    interface ITransport
+    {
+        void Man();
+    }
+
+    class Boat : ITransport
+    {
+        public void Man()
+        {
+            Console.WriteLine("Лодка плывет по реке");
+        }
+    }
+    class Man
+    {
+        public void Travel(ITransport transport)
+        {
+            transport.Man();
+        }
+    }
+    // интерфейс вертолета
+    interface IJet
+    {
+        void Move();
+    }
+    class Jet : IJet
+    {
+        public void Move()
+        {
+            Console.WriteLine("Рективный самолёт летит далеко, далёко");
+        }
+    }
+    // Адаптер от Jet к ITransport
+    class HelToTransportAdapter : ITransport
+    {
+        Jet jet;
+        public HelToTransportAdapter(Jet c)
+        {
+            jet = c;
+        }
+
+        public void Man()
+        {
+            jet.Move();
+        }
+    }
+}
